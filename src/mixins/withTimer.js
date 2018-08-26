bdc8fc18f42b45efad01284ae3b48f9546318b65
Ryan Carniato <ryansolid@gmail.com>
2018-08-25 22:30:07 -0700

    small fix for babel transpile

--- src/mixins/withTimer.js
+++ src/mixins/withTimer.js
@@ -4,7 +4,7 @@ export default createMixin( options => {
   const { element } = options;
   const timer = {
     delay(delayTime, callback) {
-      if (isFunction(delayTime)) [delayTime, callback] = [0, delayTime];
+      if (isFunction(delayTime)) [callback, delayTime] = [delayTime, 0];
       let timer = setTimeout(callback, delayTime);
       element.addReleaseCallback(() => clearTimeout(timer));
       return timer;
