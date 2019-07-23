e8284fc02d11593acc4260b5fe0be6f2bb1426d1
Ryan Carniato <ryansolid@gmail.com>
2019-07-23 10:10:10 -0700

    remove hooks as non-generalizable

--- src/hooks/useShadyCSS.js
+++
@@ -1,7 +0,0 @@
-import { getCurrentElement } from 'component-register';
-import { assignCSSId } from '../css-polyfill';
-
-export default function useShadyCSS() {
-  const element = getCurrentElement();
-  assignCSSId(element, element.nodeName)
-}
